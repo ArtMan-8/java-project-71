2cc03e32a06add8585e3edc05cf8db07a67ad37f
Art <ya.Artriches@ya.ru>
2025-02-06 21:18:43 +0300

    feature: добавил описание опÑпараметров

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,12 +5,23 @@ import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff",
         version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.",
         mixinStandardHelpOptions = true)
 public class App implements Callable<Integer> {
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    String filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    String filepath2;
+
+    @Option(names = { "-f", "--format" }, defaultValue = "stylish",
+            description = "output format [default: stylish]")
+    String format;
+
     @Override
     public Integer call() throws Exception {
         System.out.println("gendiff");
